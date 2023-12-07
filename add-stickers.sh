7430cdbf277776ddde271832f0885dd66a688a7d
dino <dinodude7777@protonmail.com>
2023-12-06 22:57:04 -0500

    make it cool and venv-supported

--- add-stickers.sh
+++ add-stickers.sh
@@ -2,6 +2,11 @@
 
 # Get the directory where the script is located
 script_dir=$(dirname "$0")
+cd "$script_dir" || exit
+
+# Create and activate the Python virtual environment
+python3 -m venv "$script_dir/.venv"
+"$script_dir/.venv/bin/pip" install -r requirements.txt  # Replace with your requirements file if needed
 
 # Define the path to the Stickers directory relative to the script location
 stickers_dir="$script_dir/Stickers"
@@ -9,17 +14,30 @@ stickers_dir="$script_dir/Stickers"
 # Define the path to the web/packs directory relative to the script location
 packs_dir="$script_dir/web/packs"
 
-# Check if the Stickers directory exists
-if [ -d "$stickers_dir" ]; then
-  # Iterate through each subdirectory in Stickers
-  for dir in "$stickers_dir"/*; do
-    if [ -d "$dir" ]; then
-      subdir=$(basename "$dir")
-      echo "Processing $subdir..."
-      sticker-pack "$stickers_dir/$subdir" --add-to-index "$packs_dir/"
-    fi
-  done
-else
-  echo "Stickers directory not found: $stickers_dir"
-  exit 1
-fi
+# Full path to the sticker-pack command within the virtual environment
+sticker_pack_command="$script_dir/.venv/bin/sticker-pack"
+
+# Execute Python code within the virtual environment
+"$script_dir/.venv/bin/python" - <<EOF "$stickers_dir" "$packs_dir" "$sticker_pack_command"
+import sys
+import subprocess
+
+# Check if the directories are provided
+if len(sys.argv) != 4:
+  print("Usage: script_name.py <stickers_dir> <packs_dir> <sticker_pack_command>")
+  sys.exit(1)
+
+stickers_dir = sys.argv[1]
+packs_dir = sys.argv[2]
+sticker_pack_command = sys.argv[3]
+
+# Iterate through each subdirectory in Stickers
+try:
+  for dir in subprocess.check_output(["ls", stickers_dir], text=True).splitlines():
+    subdir = dir.strip()
+    print(f"Processing {subdir}...")
+    subprocess.run([sticker_pack_command, f"{stickers_dir}/{subdir}", "--add-to-index", packs_dir])
+except subprocess.CalledProcessError as e:
+  print(f"An error occurred: {e}")
+  sys.exit(1)
+EOF
